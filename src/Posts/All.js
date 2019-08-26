a5f30b6d7a069f20cfd1c2b57703c8637aff2dd8
Grant Holcombe <grant.holcombe@missionbaymedia.com>
2019-08-26 12:04:18 -0700

    2 hour limit

--- src/Posts/All.js
+++ src/Posts/All.js
@@ -1,16 +1,53 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import users from 'users'
 import View from './View'
 
 const All = () => {
 
+  let [data, setData] = useState([]);
+  let [view, setView] = useState(0);
+  let [viewId, setViewId] = useState(0);
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const json = await response.json();
+      setData(data = json);
+    }
+
+    request();
+  }, []);
+
+  const allView = () => {
     return <>
-        <h1 style={{
-            textAlign: 'center',
-            fontWeight: 'lighter',
-            fontSize: '3rem',
-        }}>All Posts</h1>
+        <div className="all">
+          <h1 style={{
+              textAlign: 'center',
+              fontWeight: 'lighter',
+              fontSize: '3rem',
+          }}>All Posts</h1>
+          <ol>
+            {data.length == 0 ? <img className="loading" src="logo192.png" /> :
+              data.map((item, index) => (
+              <li key={item.id} onClick={() =>{setView(view = 1); setViewId(viewId = item.id)}} >{item.title}</li>
+            ))}
+          </ol>
+        </div>
+    </>
+  }
+
+  const singleView = (id) => {
+    return <>
+        <div className="view">
+          <View id={id} />
+          <button onClick={() => setView(view = 0)}>Back</button>
+        </div>
+    </>
+  }
+
+    return <>
+        {view !== 0 ? singleView(viewId) : allView()}
     </>
 }
 
--- src/Posts/View.js
+++ src/Posts/View.js
@@ -1,10 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import users from 'users'
 
 const View = (props) => {
 
+  let [data, setData] = useState({});
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${props.id}`);
+      const json = await response.json();
+      setData(data = json);
+    }
+
+    request();
+  }, []);
+
     return <>
+      {data.length == 0 ? <img className="loading" src="logo192.png" /> :
+        <div>
+          <h1>{data.title}</h1>
+          <h2>By {data.userId}</h2>
+          <p>{data.body}</p>
+        </div>
+      }
     </>
 }
 
